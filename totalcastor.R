133ea3e2a624f281a90c14c6e0e0d2fd2b0a3546
Daniel Sullivan <daniel.sullivan800@gmail.com>
2019-09-27 15:05:45 -0700

    This is running right now

--- totalcastor.R
+++ totalcastor.R
@@ -1,40 +1,59 @@
 require("castor")
 
+runnumber=0
 ## Not run:
 # Generate a random tree with exponentially varying lambda & mu
-for(Ntips in c(rep(20,100),rep(100,100),rep(1000,100))){
-# Ntips=20
+for(Ntips in c(rep(20000,60),rep(50000,60),rep(100000,60))){
 rho = 1 # sampling fraction
-time_grid = seq(from=0, to=100, by=0.01)
-for(lambdas in list(((10^-5)/tail(exp(10^-5*time_grid),1))*exp(10^-5*time_grid),10^-6*time_grid,rep(10^-5,length(time_grid)))){
-# lambdas=0.2*time_grid+0.5
+if(runnumber%%3==0){
+  max_val=100
+}else if(runnumber%%3==1){
+  max_val=10
+}else {
+  max_val=1
+}
+
+time_grid = seq(from=0, to = 2*ceiling(log(Ntips/rho)/max_val), by=0.1)
+# ln(max_tips)/(lambda-mu)
+lambda1 = exp(.1*time_grid)
+
+for(lambdas in list(((max_val)/tail(lambda1,1))*lambda1,(max_val/tail(time_grid,1))*time_grid,rep(max_val,length(time_grid)))){
+tryCatch({
+# print(tail(lambda1,1))
+# lambdas=((100)/tail(lambda1,1))*lambda1
 mus = 0*time_grid
 sim = castor::generate_random_tree( parameters = list(rarefaction=rho),
                                     max_tips = Ntips/rho,
                                     # as_generations = TRUE,
-                                    coalescent = FALSE,
+                                    coalescent = TRUE,
                                     added_rates_times = time_grid,
                                     added_birth_rates_pc = lambdas,added_death_rates_pc = mus)
-bigtree = sim[["tree"]]
-root_age = castor::get_tree_span(bigtree)[["max_distance"]]
-cat(sprintf("Tree has %d tips, spans %g Myr\n",length(bigtree[["tip.label"]]),root_age))
-ape::plot.phylo(bigtree)
+
+
+
+###ALL UNITS ARE IN MEGAYEARS
+###ALL UNITS ARE IN MEGAYEARS
+
+spectree = sim[["tree"]]
+root_age = castor::get_tree_span(spectree)[["max_distance"]]
+cat(sprintf("Tree has %d tips, spans %g Myr\n",length(spectree[["tip.label"]]),root_age))
+ape::plot.phylo(spectree)
 title("speciestree")
 
-Nnodes=bigtree$Nnode
+Nnodes=spectree$Nnode
 
-nspecies=length(bigtree$tip.label)
+nspecies=length(spectree$tip.label)
 
 # 
-genetreestuff = generate_gene_tree_msc(bigtree,allele_counts = 1,
-                                       population_sizes = 5*10^8,
-                                       generation_times = 0.01,
+genetreestuff = generate_gene_tree_msc(spectree,allele_counts = 1,
+                                       population_sizes = 10^8,
+                                       generation_times = 10^5,
                                        #runif(nspecies+Nnodes,min = 0.001,max=1) runif(nspecies+Nnodes,min = 10^8,max=10^9)
                                        ploidy = 1);
 
-thetree=genetreestuff$tree
+gentree=genetreestuff$tree
 
-ape::plot.phylo(thetree)
+ape::plot.phylo(gentree)
 title("genetree")
 
 
@@ -44,18 +63,19 @@ lambda_slopes = c(lambda_slopes[1],lambda_slopes)
 PDRs = lambdas - mus - (lambda_slopes/lambdas)
 # Fit PDR on grid
 Ngrid = 10
-height=max(get_all_distances_to_root(thetree))
+height=max(get_all_distances_to_root(gentree))
 age_grid = seq(0,height,length.out=Ngrid)
 # ERROR: Fitting failed: Provided age-grid range (0 - 1.20583) does not cover entire required age range (0 - 1.20583) look at lines 50-51 in fit_hbd_pdr_on_grid
-fitpdr = castor::fit_hbd_pdr_on_grid(thetree,
+fitpdr = castor::fit_hbd_pdr_on_grid(gentree,
                                      age_grid=age_grid,
-                                     min_PDR = -20,
-                                     max_PDR = +50,
+                                     min_PDR = -50,
+                                     max_PDR = +200,
                                      fixed_rholambda0 = rho*tail(lambdas,1),
+                                     # guess_PDR = tail(PDRs,1),
                                      condition = "crown",
                                      Ntrials = 10,# perform 10 fitting trials
-                                     Nthreads = 4,# use two CPUs
-                                     max_model_runtime = 1) # limit model evaluation to 1 second
+                                     Nthreads = 12,# use two CPUs
+                                     max_model_runtime = 10) # limit model evaluation to 1 second
 if(!fitpdr[["success"]]){
   cat(sprintf("ERROR: Fitting failed: %s\n",fitpdr[["error"]]))
   stop()
@@ -70,8 +90,8 @@ if(!fitpdr[["success"]]){
         type = 'b',
         col = 'red',
         xlim = c(height,0),
-        ylim=c(-5,5))
-  # xlim = c(-100,100))
+        ylim=c(-5,100))
+ 
   lines(x = seq(from=height,to=0,length.out=length(time_grid)),
         y = PDRs,
         type = 'l',
@@ -82,16 +102,16 @@ if(!fitpdr[["success"]]){
 oldest_age=height/2 # only consider recent times when fitting
 Ngrid = Ntips
 age_grid = seq(from=0,to=oldest_age,length.out=Ngrid)
-fit = fit_hbd_psr_on_grid(thetree,
+fit = fit_hbd_psr_on_grid(gentree,
                           oldest_age = oldest_age,
                           age_grid = age_grid,
                           min_PSR = -50,
-                          max_PSR = +50,
-                          guess_PSR=10^-5,
+                          max_PSR = +200,
+                          guess_PSR= tail(lambdas,1),
                           condition = "crown",
                           Ntrials = 10,# perform 10 fitting trials
-                          Nthreads = 4,# use two CPUs
-                          max_model_runtime = 1) # limit model evaluation to 1 second
+                          Nthreads = 12,# use two CPUs
+                          max_model_runtime = 10) # limit model evaluation to 1 second
 if(!fit[["success"]]){
   cat(sprintf("ERROR: Fitting failed: %s\n",fit[["error"]]))
 }else{
@@ -114,8 +134,113 @@ if(!fit[["success"]]){
         log = 'y',
         xlim = c(oldest_age,0))
 }
-lttcount=castor::count_lineages_through_time(thetree,Ntimes=100)
-plot(lttcount$times, lttcount$lineages, type="l", xlab="time", ylab="# clades")
+lttcountgen=castor::count_lineages_through_time(gentree,Ntimes=100)
+plot(lttcountgen$times, lttcountgen$lineages, type="l", xlab="time", ylab="# clades")
+
+lttcountspec=castor::count_lineages_through_time(spectree,Ntimes=100)
+plot(lttcountspec$times, lttcountspec$lineages, type="l", xlab="time", ylab="# clades")
+title()
+
+funcnumber=runnumber%%3
+runnumber=runnumber+1
+### I need to write information to file for each run, I need to index file name by index. Need to include newick strings for gene and species trees, and maybe fitted values for the pdr/psr. 
+setwd("gentrees")
+file=paste(runnumber,"_",Ntips,"_",funcnumber,".txt",sep="")
+file.create(file)
+write_tree(gentree,file)
+
+setwd("../spectrees")
+file.create(file)
+write_tree(spectree,file)
+
+setwd("..")
+
+file=paste(runnumber,"_",Ntips,"_",funcnumber,".rds",sep="")
+
+setwd("fitpsrs")
+
+saveRDS(fit,file)
+
+setwd("../fitpdrs")
+
+saveRDS(fitpdr,file)
+
+setwd("..")
+}, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
   }#close functions loop
-  rm(list = ls())
 }#close Ntips loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Fit PDR on grid for the species tree
+# Ngrid = 10
+# height=max(get_all_distances_to_root(spectree))
+# age_grid = seq(0,height,length.out=Ngrid)
+# # ERROR: Fitting failed: Provided age-grid range (0 - 1.20583) does not cover entire required age range (0 - 1.20583) look at lines 50-51 in fit_hbd_pdr_on_grid
+# fitpdr = castor::fit_hbd_pdr_on_grid(spectree,
+#                                      age_grid=age_grid,
+#                                      min_PDR = -50,
+#                                      max_PDR = +200,
+#                                      fixed_rholambda0 = rho*tail(lambdas,1),
+#                                      # guess_PDR = tail(PDRs,1),
+#                                      condition = "crown",
+#                                      Ntrials = 10,# perform 10 fitting trials
+#                                      Nthreads = 12,# use two CPUs
+#                                      max_model_runtime = 1) # limit model evaluation to 1 second
+# if(!fitpdr[["success"]]){
+#   cat(sprintf("ERROR: Fitting failed: %s\n",fitpdr[["error"]]))
+#   stop()
+# }else{
+#   cat(sprintf("Fitting succeeded:\nLoglikelihood=%g\n",fitpdr[["loglikelihood"]]))
+#   # plot fitted & true PDR
+#   plot( x = fitpdr[["age_grid"]],
+#         y = fitpdr[["fitted_PDR"]],
+#         main = 'Fitted & true PDR',
+#         xlab = 'age',
+#         ylab = 'PDR',
+#         type = 'b',
+#         col = 'red',
+#         xlim = c(height,0),
+#         ylim=c(-5,100))
+#   
+#   lines(x = seq(from=height,to=0,length.out=length(time_grid)),
+#         y = PDRs,
+#         type = 'l',
+#         col = 'blue');
+# }
