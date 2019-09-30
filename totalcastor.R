4c43dbf5fab786183e460e672a959c88838e4d69
Daniel Sullivan <daniel.sullivan800@gmail.com>
2019-09-30 14:31:24 -0700

    updated and running

--- totalcastor.R
+++ totalcastor.R
@@ -1,27 +1,30 @@
 require("castor")
 
-runnumber=0
-## Not run:
-# Generate a random tree with exponentially varying lambda & mu
-for(Ntips in c(rep(20000,60),rep(50000,60),rep(100000,60))){
-rho = 1 # sampling fraction
-if(runnumber%%3==0){
+lambdanumber=-1
+munumber=-1
+Ntipnumber=-1
+rho = .75 # sampling fraction
+
+for(Ntips in c(rep(20000,21))){
+  Ntipnumbber=Ntipnumber+1
+if(Ntipnumber%%3==0){
   max_val=100
-}else if(runnumber%%3==1){
+}else if(Ntipnumber%%3==1){
   max_val=10
 }else {
   max_val=1
 }
 
-time_grid = seq(from=0, to = 2*ceiling(log(Ntips/rho)/max_val), by=0.1)
+time_grid = seq(from=0, to = 5*ceiling(log(Ntips/rho)/max_val), by=0.1)
 # ln(max_tips)/(lambda-mu)
 lambda1 = exp(.1*time_grid)
-
-for(lambdas in list(((max_val)/tail(lambda1,1))*lambda1,(max_val/tail(time_grid,1))*time_grid,rep(max_val,length(time_grid)))){
+for(lambdas in list(((max_val/2)/tail(lambda1,1))*lambda1+max_val/2,(max_val/2*tail(time_grid,1))*time_grid+max_val/2,rep(max_val,length(time_grid)),-(max_val/2/tail(time_grid,1))*time_grid+max_val)){
+  A=1.1*lambdas[floor(length(time_grid)/2)]
+  sigma=10^-3
+  lambdanumber=lambdanumber+1
+  for(mus in list(0*time_grid,A*exp(-(time_grid-time_grid[floor(length(time_grid)/2)])^2/(2*sigma^2)),rep(max_val/3,length(time_grid)))){
+    munumber=munumber+1
 tryCatch({
-# print(tail(lambda1,1))
-# lambdas=((100)/tail(lambda1,1))*lambda1
-mus = 0*time_grid
 sim = castor::generate_random_tree( parameters = list(rarefaction=rho),
                                     max_tips = Ntips/rho,
                                     # as_generations = TRUE,
@@ -44,10 +47,18 @@ Nnodes=spectree$Nnode
 
 nspecies=length(spectree$tip.label)
 
-# 
+
+
+spectreepdrpsr = simulate_deterministic_hbd(LTT0 = length(spectree[["tip.label"]]), 
+                                            oldest_age = root_age, 
+                                            rho0 = rho, 
+                                            age_grid=time_grid,
+                                            lambda=lambdas,mu=mus)
+
+
 genetreestuff = generate_gene_tree_msc(spectree,allele_counts = 1,
                                        population_sizes = 10^8,
-                                       generation_times = 10^5,
+                                       generation_times = 10^-7,
                                        #runif(nspecies+Nnodes,min = 0.001,max=1) runif(nspecies+Nnodes,min = 10^8,max=10^9)
                                        ploidy = 1);
 
@@ -69,13 +80,12 @@ age_grid = seq(0,height,length.out=Ngrid)
 fitpdr = castor::fit_hbd_pdr_on_grid(gentree,
                                      age_grid=age_grid,
                                      min_PDR = -50,
-                                     max_PDR = +200,
-                                     fixed_rholambda0 = rho*tail(lambdas,1),
+                                     max_PDR = +150,
                                      # guess_PDR = tail(PDRs,1),
-                                     condition = "crown",
+                                     condition = "stem",
                                      Ntrials = 10,# perform 10 fitting trials
-                                     Nthreads = 12,# use two CPUs
-                                     max_model_runtime = 10) # limit model evaluation to 1 second
+                                     Nthreads = 8,# use two CPUs
+                                     max_model_runtime = max(1,Ntips/10^5)) # limit model evaluation to 1 second
 if(!fitpdr[["success"]]){
   cat(sprintf("ERROR: Fitting failed: %s\n",fitpdr[["error"]]))
   stop()
@@ -90,8 +100,8 @@ if(!fitpdr[["success"]]){
         type = 'b',
         col = 'red',
         xlim = c(height,0),
-        ylim=c(-5,100))
- 
+        ylim=c(-50,150))
+
   lines(x = seq(from=height,to=0,length.out=length(time_grid)),
         y = PDRs,
         type = 'l',
@@ -106,12 +116,12 @@ fit = fit_hbd_psr_on_grid(gentree,
                           oldest_age = oldest_age,
                           age_grid = age_grid,
                           min_PSR = -50,
-                          max_PSR = +200,
+                          max_PSR = +150,
                           guess_PSR= tail(lambdas,1),
-                          condition = "crown",
+                          condition = "stem",
                           Ntrials = 10,# perform 10 fitting trials
-                          Nthreads = 12,# use two CPUs
-                          max_model_runtime = 10) # limit model evaluation to 1 second
+                          Nthreads = 8,# use two CPUs
+                          max_model_runtime = max(1,Ntips/10^5)) # limit model evaluation to 1 second
 if(!fit[["success"]]){
   cat(sprintf("ERROR: Fitting failed: %s\n",fit[["error"]]))
 }else{
@@ -134,18 +144,19 @@ if(!fit[["success"]]){
         log = 'y',
         xlim = c(oldest_age,0))
 }
+
 lttcountgen=castor::count_lineages_through_time(gentree,Ntimes=100)
 plot(lttcountgen$times, lttcountgen$lineages, type="l", xlab="time", ylab="# clades")
+title("species tree LTT")
+
 
 lttcountspec=castor::count_lineages_through_time(spectree,Ntimes=100)
 plot(lttcountspec$times, lttcountspec$lineages, type="l", xlab="time", ylab="# clades")
-title()
+title("species tree LTT")
 
-funcnumber=runnumber%%3
-runnumber=runnumber+1
 ### I need to write information to file for each run, I need to index file name by index. Need to include newick strings for gene and species trees, and maybe fitted values for the pdr/psr. 
 setwd("gentrees")
-file=paste(runnumber,"_",Ntips,"_",funcnumber,".txt",sep="")
+file=paste(munumber,"_",Ntips,"_",lambdanumber%%4,"_",munumber%%3,".txt",sep="")
 file.create(file)
 write_tree(gentree,file)
 
@@ -155,7 +166,7 @@ write_tree(spectree,file)
 
 setwd("..")
 
-file=paste(runnumber,"_",Ntips,"_",funcnumber,".rds",sep="")
+file=paste(munumber,"_",Ntips,"_",lambdanumber%%4,"_",munumber%%3,".rds",sep="")
 
 setwd("fitpsrs")
 
@@ -165,82 +176,12 @@ setwd("../fitpdrs")
 
 saveRDS(fitpdr,file)
 
+setwd("../spectreeinfo")
+
+saveRDS(spectreepdrpsr,file)
+
 setwd("..")
-}, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
-  }#close functions loop
+}, error=function(e){cat("ERROR :",conditionMessage(e), "\n",file)})
+  }#close mus loop
+}#close lambdas loop
 }#close Ntips loop
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Fit PDR on grid for the species tree
-# Ngrid = 10
-# height=max(get_all_distances_to_root(spectree))
-# age_grid = seq(0,height,length.out=Ngrid)
-# # ERROR: Fitting failed: Provided age-grid range (0 - 1.20583) does not cover entire required age range (0 - 1.20583) look at lines 50-51 in fit_hbd_pdr_on_grid
-# fitpdr = castor::fit_hbd_pdr_on_grid(spectree,
-#                                      age_grid=age_grid,
-#                                      min_PDR = -50,
-#                                      max_PDR = +200,
-#                                      fixed_rholambda0 = rho*tail(lambdas,1),
-#                                      # guess_PDR = tail(PDRs,1),
-#                                      condition = "crown",
-#                                      Ntrials = 10,# perform 10 fitting trials
-#                                      Nthreads = 12,# use two CPUs
-#                                      max_model_runtime = 1) # limit model evaluation to 1 second
-# if(!fitpdr[["success"]]){
-#   cat(sprintf("ERROR: Fitting failed: %s\n",fitpdr[["error"]]))
-#   stop()
-# }else{
-#   cat(sprintf("Fitting succeeded:\nLoglikelihood=%g\n",fitpdr[["loglikelihood"]]))
-#   # plot fitted & true PDR
-#   plot( x = fitpdr[["age_grid"]],
-#         y = fitpdr[["fitted_PDR"]],
-#         main = 'Fitted & true PDR',
-#         xlab = 'age',
-#         ylab = 'PDR',
-#         type = 'b',
-#         col = 'red',
-#         xlim = c(height,0),
-#         ylim=c(-5,100))
-#   
-#   lines(x = seq(from=height,to=0,length.out=length(time_grid)),
-#         y = PDRs,
-#         type = 'l',
-#         col = 'blue');
-# }
