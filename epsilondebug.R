427a7893de03eda51ad3cdb17bc7746944054dc2
Daniel Sullivan <daniel.sullivan800@gmail.com>
2019-10-17 10:56:06 -0700

    epsilondebug

---
+++ epsilondebug.R
@@ -0,0 +1,23 @@
+i=1
+while(is.na(lambda_hat_p_prime[i])){
+  
+  i=i+1
+  
+}
+print(i)
+
+i=1
+while(is.na(epsilon[i])){
+  
+  i=i+1
+  
+}
+print(i)
+
+i=1
+while(spectreepdrpsr$ages[i]<0){
+  
+  i=i+1
+  
+}
+print(i)
